d0b67b25e48d5098cdf991b2fc90eb76fff53c62
Hakki Ozbey <hakkiozbey@MacBook.local>
2023-02-15 14:38:27 +0000

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast= require('./utils/forecast')
 //console.log(path.join(__dirname,'../public')) // dosyanin adresi path
 
 const app =express() // cunku express function
+const port= process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -133,7 +134,7 @@ app.get('*',(req, res)=>{  //404 page hep sonda bakmali express teker teker baki
 
 })
 
-app.listen (3000,() =>{
-    console.log('server is up on port 3000')
+app.listen (port,() =>{
+    console.log('server is up on port ' + port)
 
 }) //start up the server
